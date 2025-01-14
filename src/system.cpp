67c2b42626e03ecc355b2b55b2f7a56d87e00885
Gary Vladimir Núñez López <gary@garybricks.com>
2025-01-14 15:56:56 -0600

    returned the system's kernel id string using LinuxParser

--- src/system.cpp
+++ src/system.cpp
@@ -7,20 +7,20 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// Return the system's kernel identifier (string)
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
