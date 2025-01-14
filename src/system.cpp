2a2d61ded763cef436b9c8bbd24fe5f33db3559a
Gary Vladimir Núñez López <gary@garybricks.com>
2025-01-14 17:50:25 -0600

    returned the operating system name

--- src/system.cpp
+++ src/system.cpp
@@ -25,8 +25,8 @@ std::string System::Kernel() { return LinuxParser::Kernel(); }
 // Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
