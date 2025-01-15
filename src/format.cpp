b3a8b627aa0d9851dcd900f9d39fb7c7294c8896
Gary Vladimir Núñez López <gary@garybricks.com>
2025-01-15 17:37:27 -0600

    now considers for leading zeroes

--- src/format.cpp
+++ src/format.cpp
@@ -1,18 +1,19 @@
 #include <string>
-
+#include <iomanip>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
   int hours = seconds / 3600;
   int minutes = (seconds % 3600) / 60;
   int remaining_seconds = seconds % 60;
-  return std::to_string(hours) + ":" + std::to_string(minutes) + ":" +
-         std::to_string(remaining_seconds);
+  std::ostringstream time;
+  time << std::setw(2) << std::setfill('0') << hours << ":"
+       << std::setw(2) << std::setfill('0') << minutes << ":"
+       << std::setw(2) << std::setfill('0') << remaining_seconds;
+  return time.str();
 }
 
